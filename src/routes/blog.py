4d32ccbea65f911cf346f95ab6b73d11bab4e634
danilojezernik <dani.jezernik@gmail.com>
2023-08-10 03:57:49 +0200

    error handeling added

--- src/routes/blog.py
+++ src/routes/blog.py
@@ -9,14 +9,14 @@ from src.database import db
 blog_bp = Blueprint("blog", __name__)
 
 
-# PRIDOBI VSE OBJAVE
+# Get all blog
 @blog_bp.route("/api/blog", methods=['GET'])
 def get_blog():
     blog = dumps(db.proces.blog.find())
     return json.loads(blog)
 
 
-# DODAJ NOVO OBJAVO
+# Update blog
 @blog_bp.route("/api/blog", methods=['POST'])
 def post_blog():
     data = request.get_json()
@@ -25,20 +25,20 @@ def post_blog():
         return jsonify({'message': 'Objava uspešno dodana'})
 
 
-# PRIDOBI OBJAVO PO ID
+# Get by ID
 @blog_bp.route("/api/blog/<_id>", methods=['GET', 'POST'])
 def get_blog_id(_id):
     blog_id = dumps(db.proces.blog.find_one({'_id': ObjectId(_id)}))
     return json.loads(blog_id)
 
 
-# UREDI OBJAVO
+# Edit by ID
 @blog_bp.route("/api/blog/<int:id>", methods=['POST'])
 def edit_blog(id):
     return jsonify({'stran': f'BLOG z metodo POST: Uredi objavo {id}'})
 
 
-# IZBRIŠI OBJAVO
+# Delete by ID
 @blog_bp.route("/api/blog/delete/<_id>", methods=['DELETE'])
 def delete_blog(_id):
     try:
@@ -46,6 +46,6 @@ def delete_blog(_id):
         if result.deleted_count > 0:
             return jsonify({'message': 'Blog deleted'}), 200
         else:
-            return jsonify({'error':'Blog not found'}), 404
+            return jsonify({'error': 'Blog not found'}), 404
     except Exception as e:
         return jsonify({'error': str(e)}), 500
