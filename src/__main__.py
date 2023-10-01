5f10bb9686f43d6eee24e80e988c4c643c0446c3
danilojezernik <dani.jezernik@gmail.com>
2023-10-01 18:52:17 +0200

    refactor(): commenting main

--- src/__main__.py
+++ src/__main__.py
@@ -1,16 +1,28 @@
-import uvicorn
+"""
+This script starts a FastAPI application using Uvicorn server.
 
+Steps:
+1. Imports necessary modules and libraries.
+2. Configures FastAPI application with a base path and openapi tags.
+3. Adds CORS middleware for handling Cross-Origin Resource Sharing.
+4. Sets the secret key for the FastAPI application.
+5. Includes various routers for different functionalities (blog, login, admin, mediji).
+6. If the script is run directly (not imported), it drops the database and seeds it, then starts the Uvicorn server.
+
+Note: The FastAPI application is prefixed with '/api'.
+"""
+
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from src import env
 from src.services import db
 from src.routes import mediji, admin, login, blog
 from src.tags_metadata import tags_metadata
 
-
 app = FastAPI(prefix="/api", openapi_tags=tags_metadata)
 
+# Configure CORS settings
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -19,14 +31,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Set the secret key for the FastAPI application
 app.secret_key = env.SECRET_KEY
 
+# Include various routers for different functionalities
 app.include_router(blog.router, prefix="/blog", tags=['Blog'])
 app.include_router(login.router, prefix="/login", tags=['LogIn'])
 app.include_router(admin.router, prefix="/admin", tags=['Admin'])
 app.include_router(mediji.router, prefix="/mediji", tags=['Mediji'])
 
 if __name__ == '__main__':
+    # Drop the database and seed it
     db.drop()
     db.seed()
+
+    # Run the FastAPI application using Uvicorn server
     uvicorn.run(app, host="0.0.0.0", port=8000)
