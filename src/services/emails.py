35b0ac25902101b9d291470000a3b0da71277689
danilojezernik <dani.jezernik@gmail.com>
2023-10-25 21:00:16 +0200

    Adding comments to emails and newsletter services

--- src/services/emails.py
+++ src/services/emails.py
@@ -5,6 +5,27 @@ from src import env
 
 
 def send_email(email_from: str, subject: str, body: str) -> bool:
+    """
+    Send an email to a specified recipient.
+
+    Args:
+        email_from (str): The recipient's email address.
+        subject (str): The subject of the email.
+        body (str): The HTML content of the email.
+
+    Returns:
+        bool: True if the email was sent successfully, False otherwise.
+
+    Note:
+        This function sends an email to a specified recipient using the SMTP protocol to connect to Gmail's SMTP server.
+        It's intended for sending individual emails. Before using this function, ensure that you have set up the Gmail
+        sender email and password in the environment variables 'env.EMAIL_ME' and 'env.EMAIL_PASSWORD'. Additionally,
+        the 'env.EMAIL_ME' should be set to the same Gmail account used for SMTP login.
+
+    Dependencies:
+        - Python's smtplib module for sending emails
+    """
+    # Create an EmailMessage object
     em = EmailMessage()
     em['From'] = env.EMAIL_ME
     em['To'] = email_from
@@ -12,11 +33,14 @@ def send_email(email_from: str, subject: str, body: str) -> bool:
     em.set_content(body, subtype='html')
 
     # TODO: ALENU NASTAVI GOOGLE PASSWORD ZA DOBIVANJE EMAILOV - PASSWORD + SENDER
+    # Establish an SSL connection to Gmail's SMTP server
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-
         smtp.login(env.EMAIL_ME, env.EMAIL_PASSWORD)
-        sendemail = smtp.sendmail(env.EMAIL_ME, env.EMAIL_ME, em.as_string())
+
+        # Send the email from 'env.EMAIL_ME' to 'email_from'
+        sendemail = smtp.sendmail(env.EMAIL_ME, email_from, em.as_string())
+
         if not sendemail:
-            return True
+            return True  # Email sent successfully
         else:
-            return False
+            return False  # Failed to send the email
--- src/services/newsletter.py
+++ src/services/newsletter.py
@@ -6,9 +6,30 @@ from src.services import db
 
 
 def newsletter(subject: str, body: str) -> bool:
+    """
+    Send a newsletter email to a list of subscribers.
+
+    Args:
+        subject (str): The subject of the newsletter email.
+        body (str): The HTML content of the newsletter email.
+
+    Returns:
+        bool: True if the newsletter email was sent successfully, False otherwise.
+
+    Note:
+        This function sends a newsletter email to a list of subscribers. It uses the SMTP protocol to send the email.
+        Before using this function, make sure to set up the Gmail sender email and password in the environment variables
+        'env.EMAIL_ME' and 'env.EMAIL_PASSWORD'. Additionally, the 'env.EMAIL_ME' should be set to the same Gmail account
+        used for SMTP login.
+
+    Dependencies:
+        - Python's smtplib module for sending emails
+    """
+    # Retrieve email addresses of subscribers from the database
     cursor = db.proces.newsletter.find({}, {'email': 1})
     email_addresses = [document['email'] for document in cursor]
 
+    # Create an EmailMessage object
     em = EmailMessage()
     em['From'] = env.EMAIL_ME
     em['Subject'] = subject
@@ -19,6 +40,7 @@ def newsletter(subject: str, body: str) -> bool:
     em['CC'] = cc_recipients
 
     # TODO: ALENU NASTAVI GOOGLE PASSWORD ZA DOBIVANJE EMAILOV - PASSWORD + SENDER
+    # Establish an SSL connection to Gmail's SMTP server
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(env.EMAIL_ME, env.EMAIL_PASSWORD)
 
@@ -26,6 +48,6 @@ def newsletter(subject: str, body: str) -> bool:
         sendemail = smtp.sendmail(env.EMAIL_ME, [env.EMAIL_ME] + email_addresses, em.as_string())
 
         if not sendemail:
-            return True
+            return True  # Newsletter email sent successfully
         else:
-            return False
+            return False  # Failed to send the newsletter email
