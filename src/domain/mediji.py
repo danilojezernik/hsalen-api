a6d33adc65cf0b447074870e9932b340c6730e1c
danilojezernik <dani.jezernik@gmail.com>
2023-11-08 21:02:50 +0100

    Commit

--- src/domain/mediji.py
+++ src/domain/mediji.py
@@ -9,6 +9,8 @@ class Mediji(BaseModel):
     id: Optional[str] = Field(alias='_id', default_factory=lambda: str(ObjectId()))
     naslov_mediji: str
     opis_mediji: str
+    kraj_mediji: str
+    datum_mediji: str
     povezava_slika: str
     povezava_mediji: str
     datum_vnosa: datetime.datetime = Field(default_factory=datetime.datetime.now)
