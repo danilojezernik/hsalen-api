1b608bead05c8ae46061d384f4bb2d4886962ab1
danilojezernik <dani.jezernik@gmail.com>
2023-11-08 21:33:13 +0100

    Commit

--- src/database/mediji.py
+++ src/database/mediji.py
@@ -6,7 +6,6 @@ mediji = [
     Mediji(
         naslov_mediji='Naslov Mediji',
         opis_mediji='Opis Mediji',
-        datum_mediji='24.6.2022',
         povezava_slika='Video Mediji',
         povezava_mediji='Povezava Mediji',
         datum_vnosa=datetime.datetime.now()
