2fbd6c199a9ea1ee6f1a291092225a333bd47c4c
danilojezernik <dani.jezernik@gmail.com>
2023-11-20 14:43:56 +0100

    Changed

--- src/database/user.py
+++ src/database/user.py
@@ -2,10 +2,10 @@ from src.domain.user import User
 
 user = [
     User(
-        username='dani',
+        username='alenmaric',
         email='dani.jezernik@gmail.com',
         full_name='Danilo Jezernik',
-        hashed_password='$2b$12$06OF0P6fLEJrCSOoDBmyQuuJyX8Fiefyrm9zMA0tMOPNLwwkRDCyW',
+        hashed_password='$2b$12$3USFBYwoE3Bp9xTGO/ugxurb8S3ZUhWdXe1f4SKHLLpMGyzrKLVVu',
         disabled=False
     ).dict(by_alias=True)
 ]
