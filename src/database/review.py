20f0e2f497f534048a3c324cdf55a344281e011c
danilojezernik <dani.jezernik@gmail.com>
2023-11-25 18:59:25 +0100

    Newsxletter body changed links

--- src/database/review.py
+++ src/database/review.py
@@ -7,5 +7,19 @@ review = [
                 'sestavljati.  V hipnozi sva počistila vso mojo krivdo, ki me je spremljala leta in odpustila sem ljudem, ki so me kadarkoli '
                 'prizadeli. Predvsem pa sem spregledala, kako sama sebi stojim na poti za uresničitev življenja, kakršnega si od nekdaj želim '
                 'Še enkrat hvala Alen, hvala, ker si.',
-    ).dict(by_alias=True)
+    ).dict(by_alias=True),
+    Review(
+        name='Sabina',
+        content='Zelo sem hvaležna Alenu za hipnozo, katera mi je zelo pomagala. Predvsem sam se globoko začutila in vse se je zacelo '
+                'sestavljati.  V hipnozi sva počistila vso mojo krivdo, ki me je spremljala leta in odpustila sem ljudem, ki so me kadarkoli '
+                'prizadeli. Predvsem pa sem spregledala, kako sama sebi stojim na poti za uresničitev življenja, kakršnega si od nekdaj želim '
+                'Še enkrat hvala Alen, hvala, ker si.',
+    ).dict(by_alias=True),
+    Review(
+        name='Sabina',
+        content='Zelo sem hvaležna Alenu za hipnozo, katera mi je zelo pomagala. Predvsem sam se globoko začutila in vse se je zacelo '
+                'sestavljati.  V hipnozi sva počistila vso mojo krivdo, ki me je spremljala leta in odpustila sem ljudem, ki so me kadarkoli '
+                'prizadeli. Predvsem pa sem spregledala, kako sama sebi stojim na poti za uresničitev življenja, kakršnega si od nekdaj želim '
+                'Še enkrat hvala Alen, hvala, ker si.',
+    ).dict(by_alias=True),
 ]
