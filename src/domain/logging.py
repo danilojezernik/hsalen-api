cd125ca724cba2e6bc2df8d2f452bea431d0aff0
danilojezernik <dani.jezernik@gmail.com>
2023-12-01 10:42:24 +0100

    Added loggings for admin dashbord

--- src/domain/logging.py
+++ src/domain/logging.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel, Field
 class Logging(BaseModel):
     id: Optional[str] = Field(alias='_id', default_factory=lambda: str(ObjectId()))
     route_action: str
-    status_code: str
+    status_code: int
     client_host: str
     method: str
     content: str
